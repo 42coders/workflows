b72f0b6b4078f938acd2d2163752b500b9367ede
maxhutschenreiter <max.hutschenreiter@gmail.com>
2022-02-21 11:48:34 +0100

    adding cascading delete to workflows. Because of the change of bigint to bigint usigned columns new package doctrine/dbal is needet

---
+++ database/migrations/2022_02_21_173228_update_cascade_delete.php
@@ -0,0 +1,64 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateCascadeDelete extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        Schema::table(config('workflows.db_prefix').'task_logs', function (Blueprint $table) {
+            $table->bigInteger('task_id')->unsigned()->change();
+            $table->foreign('task_id')->references('id')->on('tasks')->onDelete('cascade');
+        });
+
+        Schema::table(config('workflows.db_prefix').'tasks', function (Blueprint $table) {
+            $table->dropIndex(['workflow_id']);
+            $table->bigInteger('workflow_id')->unsigned()->change();
+            $table->foreign('workflow_id')->references('id')->on('workflows')->onDelete('cascade');
+        });
+
+        Schema::table(config('workflows.db_prefix').'triggers', function (Blueprint $table) {
+            $table->bigInteger('workflow_id')->unsigned()->change();
+            $table->foreign('workflow_id')->references('id')->on('workflows')->onDelete('cascade');
+        });
+
+        Schema::table(config('workflows.db_prefix').'workflow_logs', function (Blueprint $table) {
+            $table->bigInteger('workflow_id')->unsigned()->change();
+            $table->foreign('workflow_id')->references('id')->on('workflows')->onDelete('cascade');
+        });
+
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table(config('workflows.db_prefix').'tasks', function (Blueprint $table) {
+            $table->dropForeign(['workflow_id']);
+        });
+
+        Schema::table(config('workflows.db_prefix').'triggers', function (Blueprint $table) {
+            $table->dropForeign(['workflow_id']);
+        });
+
+        Schema::table(config('workflows.db_prefix').'workflow_logs', function (Blueprint $table) {
+            $table->dropForeign(['workflow_id']);
+        });
+
+        Schema::table(config('workflows.db_prefix').'task_logs', function (Blueprint $table) {
+            $table->dropForeign(['task_id']);
+        });
+    }
+}
